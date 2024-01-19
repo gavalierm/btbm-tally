de239c1a44ae90da654bb7f1d56fd6e62fdec40b
gavalierm <marcel@gavalier.sk>
2024-01-19 12:12:31 +0100

    Feat: Disable all logs globaly

--- main/main.c
+++ main/main.c
@@ -32,6 +32,8 @@
 #include "driver/gpio.h"
 #include "led_strip.h"
 
+#define LOGGING 1
+
 #define ESP_WIFI_SSID       CONFIG_ESP_WIFI_SSID
 #define ESP_WIFI_PASS       CONFIG_ESP_WIFI_PASSWORD
 #define ESP_MAXIMUM_RETRY   CONFIG_ESP_MAXIMUM_RETRY
@@ -676,7 +678,7 @@ static void mqtt_app_start(void)
 
 
 static void ble_app_start(void){
-
+    ESP_LOGW(TAG, "[APP] Starting BLE sequence ....");
 }
 
 void stopESP(){
@@ -750,13 +752,18 @@ void app_main(void)
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
-    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport", ESP_LOG_VERBOSE);
-    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+    if(LOGGING){
+        esp_log_level_set("*", ESP_LOG_INFO);
+        esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+        esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
+        esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+        esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+        esp_log_level_set("transport", ESP_LOG_VERBOSE);
+        esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+    }else{
+        esp_log_level_set("*", ESP_LOG_NONE);
+    }
+
 
     ESP_ERROR_CHECK(nvs_flash_init());
     //ESP_ERROR_CHECK(esp_netif_init()); /// decalred in wifi init
